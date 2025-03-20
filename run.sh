5a545fe47c050326acdbffaa9125219d52560d56
Stephan <20800277+StephanWithPH@users.noreply.github.com>
2025-03-20 20:32:30 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v $(pwd):/solana-token -v $(pwd)/solana-data:/root/.config/solana maintoken
+docker run -it --rm -v $(pwd):/solana-token -v $(pwd)/solana-data:/root/.config/solana SolanaLaunchToken
